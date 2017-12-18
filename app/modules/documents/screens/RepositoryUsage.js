d2d0c3e1800f513703d16bd820f42a07d3b41bac
Sprite <sprite@LiangdeMacBook-Air.local>
2017-12-19 00:22:09 +0800

    chart

--- app/modules/documents/screens/RepositoryUsage.js
+++ app/modules/documents/screens/RepositoryUsage.js
@@ -11,26 +11,62 @@ import {
     Text,
     View,
     Dimensions,
+    Picker,
+    PickerIOS,
+    ScrollView
 } from 'react-native';
 import { VictoryPie } from "victory-native";
 const { width, height } = Dimensions.get('window');
-
-const data = [
-    [0, 15000000],
-    [1, 35000000],
-];
+const PickerItemIOS = PickerIOS.Item;
 
 export default class RepositoryUsage extends Component<{}> {
     static navigationOptions = {
         headerTitle: 'My Storage',
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: 'MyStorage',
+            chartData: null
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            chartData: [
+                { x: "已使用", y: 500000 },
+                { x: "剩余", y: 5000000 },
+            ]
+        })
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.title}>
-                    My Storage
-                </Text>
+            <ScrollView contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+                style={styles.container}>
+                {Platform.OS === 'android' && <Picker
+                    selectedValue={this.state.type}
+                    onValueChange={this._onValueChanged.bind(this)}
+                    style={{ marginBottom: 60, height: 100, width: 300 }}
+                    mode={Picker.MODE_DIALOG}
+                    // mode="dialog"
+                    prompt='请选择操作语言'
+                >
+                    <Picker.Item label="My Storage" value="MyStorage" />
+                    <Picker.Item label="Overall Storage (for Admin)" value="OverallStorage" />
+                </Picker>}
+
+                {Platform.OS === 'ios' && <PickerIOS
+                    selectedValue={this.state.type}
+                    onValueChange={this._onValueChanged.bind(this)}
+                    style={{ marginBottom: 60, height: 100, width: 300 }}
+                    itemStyle={{ fontSize: 25, color: 'black', textAlign: 'center', fontWeight: 'bold' }}
+                >
+                    <PickerItemIOS key='MyStorage' label="My Storage" value="MyStorage" />
+                    <PickerItemIOS key='OverallStorage' label="Overall Storage (for Admin)" value="OverallStorage" />
+                </PickerIOS>}
+
                 <View style={styles.chart}>
                     <VictoryPie
                         domainPadding={20}
@@ -39,26 +75,74 @@ export default class RepositoryUsage extends Component<{}> {
                         // y="temp"
                         interpolation="cardinal"
                         // style={{ data: { fill: '#000000', opacity: 0.7 } }}
-                        labels={(d) => `${d.x}: ${d.y}`}
-                        data={[
-                            { x: "已使用", y: 15000000 },
-                            { x: "剩余", y: 35000000 },
-                        ]}
+                        // labels={(d) => `${d.x}: ${d.y}`}
+                        data={this.state.chartData}
                     />
                 </View>
-            </View>
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    // height: 200,
+                    width: width,
+                    // backgroundColor: 'grey',
+                    margin: 10,
+
+                }}>
+                    {this.state.chartData.map(item => {
+                        return (<View
+                            style={{
+                                flex: 1,
+                                flexDirection: 'row',
+                                marginTop: 10,
+                                marginLeft: 10,
+                                // backgroundColor: 'grey',
+                            }}>
+                            <Text style={{ fontWeight: 'bold' }}>{item.x}: </Text><Text>{item.y} Bytes</Text>
+                        </View>)
+                    })}
+
+                </View>
+            </ScrollView>
         );
+    }
+
+    _onValueChanged(value) {
+        let data = [];
+        switch (value) {
+            case 'MyStorage':
+                data = [
+                    { x: "已使用", y: 500000 },
+                    { x: "剩余", y: 5000000 },
+                ];
+                break;
+            case 'OverallStorage':
+                data = [
+                    { x: "已使用", y: 15000000 },
+                    { x: "剩余", y: 35000000 },
+                ];
+                break;
+            default:
+                break;
+        }
+
+        this.setState({
+            type: value,
+            chartData: data,
+        });
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: 'column',
         backgroundColor: 'white',
     },
     chart: {
+        // flex: 1,
+        height: width * 0.9,
+        width: width * 0.9,
         // width: 600,
         // height: 200,
         // margin: 50
