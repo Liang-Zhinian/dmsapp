ba7f2d87572bff3f7e095bc669a9faadc8953aa4
Sprite <sprite@LiangdeMacBook-Air.local>
2018-03-31 23:36:33 +0800

    auth

---
+++ ios/RCTToast/Toast+UIView.h
@@ -0,0 +1,13 @@
+//
+//  Toast+UIView.h
+//  dove
+//
+//  Created by Sprite on 2018/3/31.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#ifndef Toast_UIView_h
+#define Toast_UIView_h
+
+
+#endif /* Toast_UIView_h */
---
+++ ios/RCTToast/Toast.m
@@ -0,0 +1,9 @@
+//
+//  Toast.m
+//  dove
+//
+//  Created by Sprite on 2018/3/31.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
